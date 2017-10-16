9a2119b82516882708d060b4a5a72ea78b45b33c
wangcongwu <wangcongwu@bytedance.com>
2017-10-16 15:45:03 +0800

    icon test
    Change-Id: I7fd3af7ae47b48e4862aaa8bffc383060e79b49f

--- clean/sw.js
+++ clean/sw.js
@@ -6,7 +6,7 @@ self.addEventListener('push', function(event) {
   console.log(event);
   var title = 'Yay a message.';
   var body = 'We have received a push message.';
-  var icon = 'http://cur1ous.com/webimg/30318cea775f71da67939ce6c217435b';
+  var icon = 'https://cur1ous.com/webimg/30318cea775f71da67939ce6c217435b';
   var tag = 'simple-push-demo-notification-tag';
 
   event.waitUntil(
