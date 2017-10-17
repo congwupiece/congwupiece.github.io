f884057275a95fbf33fe06c2325d2e2834510127
wangcongwu <wangcongwu@bytedance.com>
2017-10-17 15:26:35 +0800

    push
    Change-Id: I122ff5fa2d84abb0aad360d433a0fefbfcdd3018

--- clean/sw.js
+++ clean/sw.js
@@ -9,13 +9,28 @@ self.addEventListener('push', function(event) {
   var icon = '';
   var tag = 'simple-push-demo-notification-tag';
 
-  // event.waitUntil(
-    self.registration.showNotification(title, {
-      body: body,
-      icon: icon,
-      tag: tag
-    })
-  // );
+
+  var new_url = '/';
+  self.registration.pushManager.getSubscription().then(function(subscription) {
+      var mergedEndpoint = endpointWorkaround(subscription);
+      var endpointSections = mergedEndpoint.split('/');
+      var subscriptionId = endpointSections[endpointSections.length - 1];
+      fetch('https://cur1ous.com/api/push/getpushmessage/?clientid'+subscriptionId, {
+        method: 'get',
+      })
+      .then(function(response) { return response.json(); })
+      .then(function(r) {
+        var data = r.data[0];
+        self.registration.showNotification(data.text, {
+          body: data.href
+        });
+        new_url = data.href;
+      })
+      .catch(function(err) {
+        console.log('err');
+        console.log(err);
+      });
+    });
 });
 
 self.addEventListener('notificationclick', function(event) {
@@ -31,12 +46,31 @@ self.addEventListener('notificationclick', function(event) {
   }).then(function(clientList) {
     for (var i = 0; i < clientList.length; i++) {
       var client = clientList[i];
-      if (client.url === '/' && 'focus' in client) {
+      if (client.url === new_url && 'focus' in client) {
         return client.focus();
       }
     }
     if (clients.openWindow) {
-      return clients.openWindow('/');
+      return clients.openWindow(new_url);
     }
   }));
 });
+
+
+function endpointWorkaround(pushSubscription) {
+  // Make sure we only mess with GCM
+  if (pushSubscription.endpoint.indexOf('https://android.googleapis.com/gcm/send') !== 0) {
+    return pushSubscription.endpoint;
+  }
+
+  var mergedEndpoint = pushSubscription.endpoint;
+  // Chrome 42 + 43 will not have the subscriptionId attached
+  // to the endpoint.
+  if (pushSubscription.subscriptionId &&
+    pushSubscription.endpoint.indexOf(pushSubscription.subscriptionId) === -1) {
+    // Handle version 42 where you have separate subId and Endpoint
+    mergedEndpoint = pushSubscription.endpoint + '/' +
+      pushSubscription.subscriptionId;
+  }
+  return mergedEndpoint;
+}
